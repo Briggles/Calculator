c1c0faa98da7bdd4d25cff9f2e2f61d6ad150505
Mike Bregoli <briggleslol@gmail.com>
2022-07-12 21:54:47 -0400

    Created Operate function which calls the appropriate operator function, depending on operator given.

--- javascript.js
+++ javascript.js
@@ -14,6 +14,15 @@ function divide (a, b) {
     return a / b;
 }
 
-
-let textInput = document.getElementById("textInput"); 
-console.log(textInput);
+function operate (num1, num2, operator) {
+    if (operator === "+") add(num1, num2);
+    if (operator === "-") subtract(num1, num2);
+    if (operator === "*") multiply(num1, num2);
+    if (operator === "/") divide(num1, num2);
+}
+console.log(add(2, 2));
+let answer = operate(2, 2, "+");
+console.log(answer);
+//console.log(operate(2, 2, "+"));
+//let textInput = document.getElementById("textInput").value; 
+//console.log(textInput);
