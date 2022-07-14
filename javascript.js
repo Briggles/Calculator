898ae42008be9a44b226c83aa332b2a4c8a1979d
Mike Bregoli <briggleslol@gmail.com>
2022-07-13 21:06:35 -0400

    Added event listener for each button.  Testing by logging input into the console.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,31 @@
+const equalsButton = document.querySelector("#equals");
+let mathOperator = "No Operator";
+let firstOperand = 0;
+let secondOperand = 0;
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+    console.log(button.id);
+        //takeAction(button.id);    
+    })
+});
+
+/*const divs = document.querySelectorAll("div");
+
+divs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.setAttribute = "style", "background-color = black;";
+    })
+});*/
+
+equalsButton.addEventListener("click", () => {
+    const textInput = document.querySelector("#inputText");
+    inputText.value = inputText.value + " = ";
+    console.log(inputText.value);
+  });
+
 function add (a, b) {
     return (a + b);
 }
@@ -22,6 +50,8 @@ function operate (num1, num2, operator) {
 }
 let answer = operate(2, 2, "+");
 console.log(answer);
+
+
 //console.log(operate(2, 2, "+"));
 //let textInput = document.getElementById("textInput").value; 
 //console.log(textInput);
